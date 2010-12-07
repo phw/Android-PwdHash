8942986afd8cf43aaa6b26af47c263fd8dd89e3b
Philipp Wolfer <ph.wolfer@googlemail.com>
2010-12-07 22:20:48 +0100

    Basic user interface

---
+++ src/com/uploadedlobster/PwdHash/PwdHash.java
@@ -0,0 +1,33 @@
+package com.uploadedlobster.PwdHash;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class PwdHash extends Activity {
+	private EditText mSiteAddress;
+	private EditText mPassword;
+	private EditText mHashedPassword;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mSiteAddress = (EditText) findViewById(R.id.siteAddress);
+		mPassword = (EditText) findViewById(R.id.password);
+		mHashedPassword = (EditText) findViewById(R.id.hashedPassword);
+		Button generateBtn = (Button) findViewById(R.id.generateBtn);
+		
+		generateBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String hashedPassword = mSiteAddress.getText().toString() + mPassword.getText().toString();  
+				mHashedPassword.setText(hashedPassword);
+			}
+		});
+	}
+}
